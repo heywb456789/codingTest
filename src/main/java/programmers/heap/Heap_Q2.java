53ea7f9308ec417df2abd97f7915106a7f21a5af
김우빈 <heywb457@gmail.com>
2021-04-28 17:53:59 +0900

    heap_q2 진행중

---
+++ src/main/java/programmers/heap/Heap_Q2.java
@@ -0,0 +1,29 @@
+package programmers.heap;
+
+class Jobs{
+    int requestTime;
+    int workingTime;
+
+    public Jobs(int requestTime, int workingTime){
+        this.requestTime = requestTime;
+        this.workingTime = workingTime;
+    }
+
+
+}
+public class Heap_Q2 {
+    public static void main (String[] args){
+        int[][] jobs = {{0,3},{1,9},{2,6}};
+
+        int result = solution(jobs);
+        System.out.println(result);
+    }
+
+    private static int solution(int[][] jobs) {
+        int answer =0;
+
+
+
+        return answer;
+    }
+}
