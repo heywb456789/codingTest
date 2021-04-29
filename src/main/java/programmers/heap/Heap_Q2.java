a3d2fcf4f2f9c46a29f224b9cfa0c1a5fea09eaa
김우빈 <heywb457@gmail.com>
2021-04-29 11:23:48 +0900

    heap_q2

--- src/main/java/programmers/heap/Heap_Q2.java
+++ src/main/java/programmers/heap/Heap_Q2.java
@@ -1,29 +1,40 @@
 package programmers.heap;
 
-class Jobs{
-    int requestTime;
-    int workingTime;
+import java.util.Arrays;
+import java.util.PriorityQueue;
 
-    public Jobs(int requestTime, int workingTime){
-        this.requestTime = requestTime;
-        this.workingTime = workingTime;
-    }
-
-
-}
 public class Heap_Q2 {
     public static void main (String[] args){
-        int[][] jobs = {{0,3},{1,9},{2,6}};
+        int[][] params = {{0,3},{2,6},{1,9}};
 
-        int result = solution(jobs);
+        int result = solution(params);
         System.out.println(result);
     }
 
     private static int solution(int[][] jobs) {
-        int answer =0;
+        int answer = 0;
+		int len = jobs.length; // 수행되고난 직후의 시간
+		int time = 0; // jobs 배열의 인덱스
+		int idx = 0; // 수행된 요청 갯수
 
+        // 요청시간 오름차순 정렬
+        Arrays.sort(jobs,(o1, o2) -> o1[0] - o2[0]);
 
+        // 처리 시간 오름차순으로 정렬되는 우선순위 큐(Heap)
+		PriorityQueue<int[]> pq = new PriorityQueue<>((o1, o2) -> o1[1] - o2[1]);
 
-        return answer;
+        while(idx < len || !pq.isEmpty()){
+            while (idx < len && jobs[idx][0] <= time){
+                pq.offer(jobs[idx++]);
+            }
+            if(pq.isEmpty()){
+                time = jobs[idx][0];
+            }else{
+                int[] job = pq.poll();
+                answer += time -job[0] + job[1];
+                time += job[1];
+            }
+        }
+        return answer / len;
     }
 }
