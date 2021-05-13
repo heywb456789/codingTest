acaab3513d910209b8a0803208431768c066221c
김우빈 <heywb457@gmail.com>
2021-05-13 11:09:32 +0900

    String_Q12 완료

---
+++ src/main/java/inflearn/string/String_Q12.java
@@ -0,0 +1,33 @@
+package inflearn.string;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class String_Q12 {
+
+    public static void main(String[] args){
+        String_Q12 q  = new String_Q12();
+        Scanner sc = new Scanner(System.in);
+
+        int size = sc.nextInt();
+        String secret = sc.next();
+
+        System.out.println(q.solution(size,secret));
+    }
+
+    private String solution(int size , String secret){
+        String answer = "";
+
+        size = size * 7 ;
+        for(int i=0 ; i<size ; i= i+7){
+            String tmp = secret.substring(i,i+7);
+            tmp= tmp.replaceAll("[#]","1");
+            tmp= tmp.replaceAll("[*]","0");
+            int a = Integer.parseInt(tmp,2);
+            char b = (char) a;
+            answer += String.valueOf(b);
+        }
+        return answer;
+    }
+}
